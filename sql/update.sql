6d625b199cd12a608d75b64d48e2eb399fbe5d6d
mike2mars <michael.larboni@gmail.com>
2020-01-08 18:26:26 +0100

    add update

--- sql/update.sql
+++ sql/update.sql
@@ -9,3 +9,12 @@ alter table userversion rename column userrid to userid;
 alter table cramuser add column if not exists email varchar;
 alter table cramuser add column if not exists name varchar(20);
 alter table cramuser add column if not exists lastname varchar(20);
+create or replace function createuserversion() returns trigger
+    language plpgsql
+as
+$$
+begin
+    insert into userversion values(new.userid, 0) ;
+    return null;
+end;
+$$;
