3bf32be3c25f43a27f71d1c4497f01eb33cea3cb
MEUNIER Jean-charles <jean-charles.meunier@lam.fr>
2020-01-13 10:26:48 +0000

    Fix update.sql 'add column statement

--- sql/update.sql
+++ sql/update.sql
@@ -6,9 +6,9 @@ alter table task rename column userrid to userid;
 alter table teamleader rename column userrid to userid;
 alter table teamuser rename column userrid to userid;
 alter table userversion rename column userrid to userid;
-alter table cramuser add column if not exists email varchar;
-alter table cramuser add column if not exists name varchar(20);
-alter table cramuser add column if not exists lastname varchar(20);
+alter table cramuser add column email varchar;
+alter table cramuser add column name varchar(20);
+alter table cramuser add column lastname varchar(20);
 create or replace function createuserversion() returns trigger
     language plpgsql
 as
